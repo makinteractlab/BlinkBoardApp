5e3153d6d66a1644816e6150e6957110417598f6
Andrea Bianchi <andrea@kaist.ac.kr>
2020-08-25 18:42:41 +0900

    Added breadboard image

--- assets/js/breadboard.js
+++ assets/js/breadboard.js
@@ -1,10 +1,12 @@
+let mySvg;
+
 const sketch = new p5( p => {
 
     p.setup = () => {
       // canvas size is specified in the CSS file (size of div #one)
       p.createCanvas($("#mainSketch").width(), $("#mainSketch").height());
 
-
+      mySvg = p.loadImage("assets/images/breadboard.svg");
 
     };
     
@@ -13,9 +15,19 @@ const sketch = new p5( p => {
       p.background('#F2F2F2');      
       p.fill(255,0,0);
       p.noStroke();
-      p.rectMode(p.CENTER);
-      p.rect(p.mouseX, p.height/2, 100, 100);
-  
+
+      p.rect(p.mouseX, p.mouseY, 50, 10)
+
+      
+      p.imageMode(p.CENTER);
+      // p.rect(p.mouseX, p.height/2, 100, 100);
+
+      p.push();
+      p.translate(p.width/2, p.height/2);
+      p.scale(0.5)
+      p.image(mySvg, 0, 0)
+      p.pop();
+
     };
 
     p.mousePressed = () => {
