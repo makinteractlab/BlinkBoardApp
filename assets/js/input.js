fa214de2b15044a6b5bbed1f2c9c4f7d5dd9ef35
Andrea Bianchi <andrea@kaist.ac.kr>
2020-09-06 08:43:21 +0900

    Output panel

--- assets/js/input.js
+++ assets/js/input.js
@@ -58,11 +58,122 @@ class InputChart {
 
 
 
+class OutDigitalChannel{
+
+    constructor(chNumber) // 0, 1, 2
+    {
+        this.channel = chNumber;
+        this.status= $(`#d${chNumber}status`);
+        this.low= $(`#d${chNumber}low`);
+        this.high= $(`#d${chNumber}high`);
+        this.pwm= $(`#d${chNumber}pwm`);
+        this.slider= $(`#d${chNumber}slider`);
+        this.duty= 1;
+
+        this.low.change( () => {
+            this.status.text("LOW");
+            this.removeClassByPrefix(this.status[0], "accent");
+            this.slider.attr('disabled', true);
+
+            this.setLow();
+        });
+
+        this.high.change( () => {
+            this.status.text("HIGH");
+            $("accent").removeClass();
+            this.removeClassByPrefix(this.status[0], "accent");
+            this.status.addClass("accentRed");
+            this.slider.attr('disabled', true);
+
+            this.setHigh();
+        });
+
+        this.pwm.change( () => {
+            this.status.text(`${this.duty}%`);
+            this.slider.removeAttr('disabled');
+            this.removeClassByPrefix(this.status[0], "accent");
+            this.status.addClass("accentBlue");
+
+            this.setPwm();
+        });
+
+        this.slider.on('input change', () => {
+            this.duty= this.slider.val();
+            this.status.text(`${this.duty}%`);
+
+            this.setPwm();
+        });
+
+    }
+
+    removeClassByPrefix(el, prefix) {
+        var regx = new RegExp('\\b' + prefix + '.*?\\b', 'g');
+        el.className = el.className.replace(regx, '');
+        return el;
+    }
+
+    setLow()
+    {
+        writeJsonToPort({
+            "cmd": "setLow",
+            "pin": `D${this.channel}`
+        });
+    }
+
+    setHigh()
+    {
+        writeJsonToPort({
+            "cmd": "setHigh",
+            "pin": `D${this.channel}`
+        });
+    }
+
+    setPwm()
+    {
+        writeJsonToPort({
+            "cmd": "setPwm",
+            "pin": `D${this.channel}`,
+            "duty": this.duty
+        });
+    }
+}
+
+class OutAnalogChannel {
+
+    constructor(chNumber){
+        this.channel = chNumber;
+        this.status= $(`#d${chNumber}status`);
+        this.slider= $(`#d${chNumber}slider`);
+        this.volt= 0;
+
+        this.slider.on('input change', () => {
+            this.volt= this.slider.val();
+            this.status.text(`${this.volt}V`);
+
+            // set voltage
+            writeJsonToPort({
+                "cmd": "setV",
+                "value": this.volt*1000
+            });
+        });
+    }
+
+    
+}
+
+
+
+
+
+
 
 $(document).ready(function () {
 
     new InputChart();
    
-    
+    new OutDigitalChannel(0);
+    new OutDigitalChannel(1);
+    new OutAnalogChannel(2);
+
 
 });
